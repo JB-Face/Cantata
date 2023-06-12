b250497c6c3abfd0bc3d18ae80882db42d06d058
JBFace <573765174@qq.com>
2023-06-12 18:20:51 +0800

    <fix>:基本的贴图压缩工具

--- imagesTool.py
+++ imagesTool.py
@@ -4,7 +4,7 @@ version:
 Author: JBFace
 Date: 2023-06-02 17:48:22
 LastEditors: JBFace
-LastEditTime: 2023-06-11 23:52:12
+LastEditTime: 2023-06-12 18:18:06
 '''
 
 
@@ -19,13 +19,13 @@ except  ImportError:
     import pip
     pip.main(["install", "--user", "pillow"])
 
-
-
+from Cantata import debug
+import time
 
 class textureconver(Tool):
     type ='贴图'
-    name ='贴图库转换'
-    info ='将Blender贴图库转换为UE格式'
+    name ='贴图缩放'
+    info ='相对或者绝对缩放贴图分辨率'
 
     def __init__(self) -> None:
         super().__init__()
@@ -35,18 +35,28 @@ class textureconver(Tool):
 
     def draw(self):
         self.file_path = None
-        self.file_path = GUI.file_select('select',self.main_widget)
-        self.check_box = GUI.check_box('is check',self.main_widget)
-        self.input = GUI.string_input('input',self.main_widget)
-        GUI.run_button('run',self.main_widget,self.execute)
+        self.file_path = GUI.file_select('选择文件夹',self.main_widget)
+        self.check_box = GUI.check_box('相对缩放',self.main_widget)
+        self.input = GUI.string_input('缩放大小/比例',self.main_widget)
+
+        self.suffix = GUI.string_input('后缀',self.main_widget)
+        GUI.run_button('run',self.main_widget,self.start)
         
         
         # self.main_widget.addWidget() 
     def execute(self):
         path = self.file_path()
-        debug(path)
-        debug(self.check_box())
-        debug(self.input())
+
+        suffix= self.suffix()
+        if suffix == '后缀':
+            suffix = None 
+
+        self._convertTextureSiez(inpath=path,
+                                 size = self.input(),
+                                 suffix= suffix,
+                                 relatively = self.check_box()
+                                 )
+
 
         #_convertTextureSiez(inpath=path,size=100,suffix='none',False)
 
@@ -88,10 +98,33 @@ class textureconver(Tool):
             outfile = _get_outfile(infile, outfile)       
             out.save(outfile)
 
+        file_list = []
         for root, dirs, files in os.walk(inpath):
             for file in files:
                 if file.split('.')[-1] in _imagelist:
                         infile = os.path.join(root,file)
                         out = file.replace('.',str(suffix) + '.')
                         outfile = os.path.join(root,out)
-                        _resize_image(infile,outfile,size,relatively)
+                        d = {
+                            'infile' : infile,
+                            'outfile': outfile,
+                            'size': size,
+                            'relatively' : relatively,
+                            'name' : file
+                        }
+
+                       
+                        file_list.append(d)
+
+        maxpro = len(file_list)
+
+        if len(file_list) > 0:
+            pro = 100.0/maxpro
+            pro_num = 0
+            for i in file_list:
+                _resize_image(i['infile'],i['outfile'],i['size'],i['relatively'])
+                pro_num = pro_num +1
+                self.bar_progress(int(pro * pro_num))
+                self.bar_log(str(i['name']))
+        else:
+            self.bar_log('没有贴图处理',color='red')
